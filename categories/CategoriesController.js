edd3488e975b1957e9b830e2c81beff04c5f2682
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-06-18 07:41:45 -0300

    Cadastro de categorias e Slugify

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const slugify = require('slugify');
 const router = express.Router();
+const Category = require('./Category')
 
 router.get("/admin/categories/new", (req, res) => {
-    res.render("admin/categories/new")
+    res.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if(title){
+
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/");
+        });
+            
+    }else{
+        res.redirect("/admin/categories/new");
+    };
 });
 
 module.exports = router;
