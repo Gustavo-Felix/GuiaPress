86e843d2a67652a477468ddb5782b35c2bd9bb67
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-07-01 07:59:29 -0300

    Página de edição de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -54,5 +54,39 @@ router.post("/articles/delete", (req, res) => {
     }
 });
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        return res.redirect("/admin/articles");
+    }
+
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+            Category.findAll().then((categories) => {
+                res.render("admin/articles/edit", {article: article, categories: categories});
+            });
+        }else{
+            res.redirect("/admin/articles");
+        }
+    }).catch((erro) => {
+        res.redirect("/admin/articles");
+    });
+});
+
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var categoriaId = req.body.category;
+
+    Article.update({title: title, slug: slugify(title), body: body, categoryId: categoriaId}, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    });
+});
 
 module.exports = router;
